79882a2fda5c4bdcbea851ddacd40606fafc9e3e
Em Pandian <162124150+EsakkimuthuPandian@users.noreply.github.com>
2026-07-21 20:51:50 +0530

    Update YDHW-MS-S_Project.py

--- YDHW-MS-S_Project.py
+++ YDHW-MS-S_Project.py
@@ -1,6 +1,6 @@
                                                           # Needed Modules
 
-
+import googleapiclient.discovery
 from pprint import pprint
 import pandas as pd
 import pymongo as pm
@@ -15,4 +15,21 @@ api_version = "v3"
 youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey = api_key)
 
                                                         # Getting Channel Details    
+request = youtube.channels().list(
+    part="snippet,contentDetails,statistics",
+    id= Channel_id
+)
 
+response = request.execute()
+
+def get_channel_details (channel_response):
+    for values in channel_response["items"]:
+        return {
+                 "channel_details": {
+                    "Channel_Name": values["snippet"]["title"],
+                    "Channel_Id": values["id"],
+                    "Subscription_Count":int(values["statistics"]["subscriberCount"]),
+                    "Channel_Views": int(values["statistics"]["viewCount"]),
+                    "Channel_Description": str.replace(values["snippet"]["description"], "\n"," ",),
+                    "Playlist_Id":values["contentDetails"]["relatedPlaylists"]["uploads"]}
+                }
